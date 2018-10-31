6c7b1550a1e7319a4671a97c7cb29ed02402229c
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-10-31 11:00:45 +0100

    Initial Commit

---
+++ AVVideoComposition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVVideoComposition
+//
+//  Created by Diego Caroli on 31/10/2018.
+//  Copyright © 2018 Diego Caroli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
