e4ffc3a2222fba8b57200bc800ae4fe05eb43395
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-10-31 11:13:28 +0100

    Add two test video and organise structure.

---
+++ AVVideoComposition/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  AVVideoComposition
+//
+//  Created by Diego Caroli on 31/10/2018.
+//  Copyright © 2018 Diego Caroli. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
